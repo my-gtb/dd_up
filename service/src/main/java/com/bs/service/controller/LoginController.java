89d7c8c9b81c3cde2185826ee553e029509c633b
my-gtb <huanghe_166@163.com>
2021-03-04 12:44:08 +0800

    完善

--- service/src/main/java/com/bs/service/controller/LoginController.java
+++ service/src/main/java/com/bs/service/controller/LoginController.java
@@ -15,6 +15,11 @@ public class LoginController {
 
     @GetMapping(value = "info")
     public R info(){
-        return R.ok().data("roles","[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        return R.ok().data("roles","admin").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+
+    @PostMapping(value = "logout")
+    public R logout(){
+        return R.ok().data("token","");
     }
 }
